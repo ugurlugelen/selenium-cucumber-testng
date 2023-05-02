6c7bf2cc46ca37650bb69cd262f83848a044c51e
ugurlugelen <augurlugelen@gmail.com>
2023-05-02 14:00:25 +0300

    dependabot added

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -5,7 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class TestBase {
@@ -32,6 +37,19 @@ public class TestBase {
 
                 case "firefox":
                     driver = new FirefoxDriver();
+                    break;
+
+                case "remote-chrome":
+                    DesiredCapabilities capabilities = new DesiredCapabilities();
+                    capabilities.setBrowserName(Browser.CHROME.browserName());
+                    capabilities.setVersion("111");
+                    try {
+                        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+                    } catch (MalformedURLException e) {
+                        throw new RuntimeException(e);
+                    }
+                default:
+                    driver = new ChromeDriver();
                     break;
             }
         }
